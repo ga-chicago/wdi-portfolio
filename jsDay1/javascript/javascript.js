f45fe8138457fa26bc3cdc7b8ed2df742455e6e3
Bridjet Mendyuk <bjmendy@Bridjets-MBP.attlocal.net>
2017-02-01 23:44:40 -0600

    a message

---
+++ jsDay1/javascript/javascript.js
@@ -0,0 +1,52 @@
+console.log('window is loaded')
+
+// Javascript data types
+
+
+// 1. string
+// 2. boolean
+// 3. object
+// 4. number
+// 5. null
+// 6. undefined
+"this is a string"
+
+//booleans
+var x = 2;
+//control flow
+if (1 === x) {
+	console.log(' 1 === 2 yayyyyy')
+}
+else {
+	console.log('1 does not equal x')
+};
+
+
+var foods = ["ramen", "soba", "crab rangoon", "tempura"];
+if (foods[0].toLowerCase() === "ramen") {
+	console.log('yum ramen')
+}
+else if (foods[1] === "soba"){
+	console.log('yum soba')
+}
+else if (foods [2] === "crab rangoon"){
+	console.log('yum' + foods[2])
+}
+else {
+	console.log("that food is gone")
+}
+
+
+if (x % 2 === 0){
+	console.log('yum ramen')
+}
+// foods.shift();
+foods [0] = ('yum ramen')
+}
+
+
+
+
+
+
+
