5c676af93712349978b7f40aa24feafe2d7362be
John Favors <JohnnyFavorite@Johns-MacBook-Pro.local>
2017-02-18 13:49:54 -0600

    reworked html

--- project1/js/app.js
+++ project1/js/app.js
@@ -12,117 +12,125 @@ $('window').scroll(function(){
 	}
 });
 
+//Global Variables
+playDeck = [1/1, 1/2, 1/3, 1/4, 1/5, 1/5, 2/2, 2/4, 3/6, 4/8, 5/10];
 
-									//define global variables
-									//think about all of the elements of the game
-//smCard is object w/properties 
-//lgCard is object w/properties
-var easyBtn = $('<button id = "easyBtn"'),
-var hardBtn = $('<button id = "hardBtn"'),
-var startBtn = $('<button id = "start"'),
-var easyBtnGrey = $('<button id = "easyBtnGrey">'),
-var hardBtnGrey = $('<button id = "hardBtnGrey"'),
-var startBtnGrey = $('<button id = "startBtnGrey"'),
+boardDeck = [2/4, 3/6, 4/8, 5/10];
 
-var countdown function (){					//called when user clicks start
-	var sec = 15; 							//....default
-	var timer = setInterval(function(){
-	$('###div.timer').text(sec--)
-	if (sec == -1){
-		scoreIt();
-		resetStartButton();
-		$('###div.timer').fadeOut('slow')
-		clearInterval(timer);
-		console.log(sec);
-	}
-}, 1000);				
-}
+//Create Board
 
-var timer = setInterval(function(){
-	$('###div.countdown').text(sec--)
-	if (sec == -1){
-		scoreIt();
-		resetStartButton();
-		$('###div.countdown').fadeOut('slow')
-		clearInterval(timer);
-		console.log(sec);
-	}
-}, 1000);
+var board = $('<div>')
+
+//Create Card in Play Area
+
+//Create Button & Timer Area
+
+
+//CREATE TIMER 
+//var timeClock = $('<div>');
+
+// var countdown function (){					//called when user clicks start
+// 	var sec = 15; 							//....default
+// 	var count = setInterval(function(){
+// 	$('timer.count').text(sec--)
+// 	if (sec == -1){
+// 		scoreIt();
+// 		resetStartButton();
+// 		$('timer.count').fadeOut('slow')
+// 		clearInterval(count);
+// 		console.log(sec);
+// 	}
+// }, 1000);			
+//  ('inPlay').append('timeClock')	
+// }
+
+//CREATE BUTTONS
+var easyBtn = $('<button>EASY</button>').click(function(){
+	buttons.append(easyBtn);
+
+
+	console.log('easyBtn clicks!')
+})
+
+easyBtn.prop('id', 'easyBtn')
+//$('sec') = 7;
+//add functionality
+//append to buttonsDiv[0]
+
+var startBtn = $('<button>');
+startBtn.prop('id, startBtn');
+//countdown().click;
+//add functionality
+//append to buttonsDiv[1]
+
+var hardBtn = $('<button>');
+hardBtn.prop('id', 'hardBtn')
+//add functionality
+//append to buttonsDiv[2]
+
+var easyBtnGrey = $('<button>');
+easyBtnGrey.addClass('nofunct');
+//add to buttonsDiv[0] default hide
+
+var startBtnGrey = $('<button>');
+startBtnGrey.addClass('nofunct');
+//add to buttonDiv[1] default hide
+
+var hardBtnGrey = $('<button>');
+hardBtnGrey.addClass('nofunct');
+//add to buttonsDiv[2] default hide
+
+
 //board is an object w/properties
 //inPlay is an object w/properties
 //inPlayDeck is array of card values
 //boardDeck is array of arrays card values
-var score = 0, 						
+//var score = 0, 						
 //you rock is an function
 
+//DISABLE BUTTONS
+// var buttonsDisable = function(){	//Function to disable the all the buttons
+// 	$('easyBtnGrey').unhide;		//not called until user presses start
+// 	$('hardBtnGrey').unhide;
+// 	$('StartBtn').undhide;
+// 	$('easyBtn').hide;
+// 	$('hardBtn').hide;
+// 	$('StartBtn').hide;
+// },
 
-var buttonsDisable = function(){	//Function to disable the all the buttons
-	$('easyBtnGrey').unhide;		//not called until user presses start
-	$('hardBtnGrey').unhide;
-	$('StartBtn').undhide;
-	$('easyBtn').hide;
-	$('hardBtn').hide;
-	$('StartBtn').hide;
-},
-
-$('#startBtn').start(function){		//function that starts the game and 
-	var startGame = $('<button>', 		//hides buttons that can't ge used
-	{text: 'START', 
-	click: function(){
-		setInterval(timer);		
-		$('easyBtnGrey').unhide;	
-		$('hardBtnGrey').unhide;	
-		$(this.btn).hidden			
-		$('startGreyBtn').hidden;
-	}	
-	})	
-}
+//START GAME
+// $('#startBtn').start(function){		//function that starts the game and 
+// 	var startGame = $('<button>', 		//hides buttons that can't ge used
+// 	{text: 'START', 
+// 	click: function(){
+// 		setInterval(timer);		
+// 		$('easyBtnGrey').unhide;	
+// 		$('hardBtnGrey').unhide;	
+// 		$(this.btn).hidden			
+// 		$('startGreyBtn').hidden;
+// 	}	
+// 	})	
+// }
 							
-startGame();
-
-
-
-							//create a countdown timer in seconds that starts at a default of 15
-							//the timer must coundown to 0.  Every interval it should check to see if 
-							//it hit 0 and call the score it function at 0.  It should call the 
-							//scoreIt function and the resetStartButton function at the end.
-//var sec = 15 //default
-// var timer = setInterval(function(){
-// 	$('###div.countdown').text(sec--)
-// 	if (sec == -1){
-// 		scoreIt();
-// 		resetStartButton();
-// 		$('###div.countdown').fadeOut('slow')
-// 		clearInterval(timer);
-// 		console.log(sec);
-// 	}
-// }, 1000);
-
+// startGame();
 
 
 
 //USER STORY:
-//user presses start button to begin game
+// 1. User presses Start
+// 2. The can leave the default of easy or press hard to change the timer 
+// 3. The clock begins to count down
+// 4. The user has to click all of the equivalent fractions on the board 
+//    set qual to the card in play before time runs time runs fadeOut
+// 5. Each correct answer turns the card green and adds 100 points
+// 6. Each wrong answer turns the card red and subtracts 75 points
+// 7. When the clock hits zero the score is flashed and the button to 
+//    play again appears
 
 
 
-//the first card flips on the large deck and all cards flip on the board
 
 
-//the easy and start buttons turn grey and function goes off
-
-//the start button hides and is repalced by a grey non-functional button
-//until the timer = 0
-
-//the game timer begins when the timer ends the score is caculated and unhides
-
-
-//the user plays by clicking cards.  If the small card on the board is equivalent 
-//to the large card in play the card turns green and score is increased by 100
-// if the card is not equal the card turns red and score is decreased by 50
-
-//when the timer hits zero the user can reset the game to a new board
-//a green reset button will unhide and the grey start button will hide
 
  
 
