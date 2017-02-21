698405e6f323face57d9bb7b6596016a398ec535
John Favors <JohnnyFavorite@Johns-MacBook-Pro.local>
2017-02-21 11:43:14 -0600

    trans to click/drag

--- project1/js/app.js
+++ project1/js/app.js
@@ -2,24 +2,85 @@ console.log('JS is loaded!');
 
 //create the basic DOM structure in HTML add other features dynamically
 
-//create a sticky header so the header remains as user scrolls
-$('window').scroll(function(){
-	if($(this).scrollTop() > 1){
-		$('header').addClass("sticky");
-	}
-	else{
-		$('header').removeClass("sticky");
-	}
-});
+// create a sticky header so the header remains as user scrolls
+// $('window').scroll(function(){
+// 	if($(this).scrollTop() > 1){
+// 		$('header').addClass("sticky");
+// 	}
+// 	else{
+// 		$('header').removeClass("sticky");
+// 	}
+// });
 
 //Global Variables
-playDeck = [1/1, 1/2, 1/3, 1/4, 1/5, 1/5, 2/2, 2/4, 3/6, 4/8, 5/10];
 
-boardDeck = [2/4, 3/6, 4/8, 5/10];
+//Load and Initiate the Game
+var matches = 0
+var score = function {
+	matches * 50;
+}
+$(start);
+
+function $(start) {
+
+	//hide-unhide winner message
+	$('winner').hide();
+	$('winner').css({
+		color: Green;
+	})
+
+//Reset game cards empty 
+$('cards') = ();
+$('equals')	= ();
+
+}
+
+//Create row of card slots to drag to #board
+
+
+//Create row of cards to drap #inPlay
+
+
+//function to check each match and drop cards
+
+
+
+
+//Define the cards
+// var numerator = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// var denominator = [0, 1., 2, 3, 4, 5, 6, 7, 8, 9];
+// var slash = "__";
+// var value = []; 
+// var count = 0;
+
+
+
+//Create cards to play (#inPlay)
+// function makeCardDeck(margin){
+// 	var left = 0;
+// 	var step = margin;
+// 	var s = 0
+
+// $('.cards').each(function(){	
+// 	$(this).css({'z-index' : s});
+// 	$(this).css({'margin-left' :left + 'px'});
+// 	$(this).css({'margin-top' :0 + 'px'});
+// 	left = left + step;
+// 	s++;
+// });
+// }
+
+//Create board to drag cards to (#board)
+
+//Create Shuffle Function (run inside start function)
+
+
+
+//Create Deal function (run inside start function)
 
 //Create Board
+//Create 12 playing cards on board
 
-var board = $('<div>')
 
 //Create Card in Play Area
 
@@ -27,58 +88,34 @@ var board = $('<div>')
 
 
 //CREATE TIMER 
-//var timeClock = $('<div>');
-
-// var countdown function (){					//called when user clicks start
-// 	var sec = 15; 							//....default
-// 	var count = setInterval(function(){
-// 	$('timer.count').text(sec--)
-// 	if (sec == -1){
-// 		scoreIt();
-// 		resetStartButton();
-// 		$('timer.count').fadeOut('slow')
-// 		clearInterval(count);
-// 		console.log(sec);
-// 	}
+// var timeClock = $('<div>');
+// 	var countdown = function (){					//called when user clicks start
+// 		var sec = 15; 							//....default
+// 		var count = setInterval(function(){
+// 		$('timer.count').text(": " + sec--)
+// 		if (sec == -1){
+// 			scoreIt();
+// 			resetStartButton();
+// 			$('timer.count').fadeOut('slow')
+// 			clearInterval(count);
+// 			console.log(sec);
+// 		}
 // }, 1000);			
-//  ('inPlay').append('timeClock')	
+//  ('timer').append('timeClock')	
 // }
 
 //CREATE BUTTONS
-var easyBtn = $('<button>EASY</button>').click(function(){
-	buttons.append(easyBtn);
+// var easyBtn = $('<button>EASY</button>').click(function(){
+// 	easyBtn.prop('id', 'easyBtn')
+// 	buttons.append(easyBtn);
 
 
-	console.log('easyBtn clicks!')
-})
+// 	console.log('easyBtn clicks!')
+// }
 
-easyBtn.prop('id', 'easyBtn')
 //$('sec') = 7;
 //add functionality
 //append to buttonsDiv[0]
-
-var startBtn = $('<button>');
-startBtn.prop('id, startBtn');
-//countdown().click;
-//add functionality
-//append to buttonsDiv[1]
-
-var hardBtn = $('<button>');
-hardBtn.prop('id', 'hardBtn')
-//add functionality
-//append to buttonsDiv[2]
-
-var easyBtnGrey = $('<button>');
-easyBtnGrey.addClass('nofunct');
-//add to buttonsDiv[0] default hide
-
-var startBtnGrey = $('<button>');
-startBtnGrey.addClass('nofunct');
-//add to buttonDiv[1] default hide
-
-var hardBtnGrey = $('<button>');
-hardBtnGrey.addClass('nofunct');
-//add to buttonsDiv[2] default hide
 
 
 //board is an object w/properties
@@ -118,7 +155,7 @@ hardBtnGrey.addClass('nofunct');
 
 //USER STORY:
 // 1. User presses Start
-// 2. The can leave the default of easy or press hard to change the timer 
+// 2. The usercan leave the default of easy or press hard to change the timer 
 // 3. The clock begins to count down
 // 4. The user has to click all of the equivalent fractions on the board 
 //    set qual to the card in play before time runs time runs fadeOut
@@ -126,8 +163,6 @@ hardBtnGrey.addClass('nofunct');
 // 6. Each wrong answer turns the card red and subtracts 75 points
 // 7. When the clock hits zero the score is flashed and the button to 
 //    play again appears
-
-
 
 
 
