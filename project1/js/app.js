3c3b85bf22302d1caaf700c2b27d23438d950019
John Favors <JohnnyFavorite@Johns-MacBook-Pro.local>
2017-02-23 15:38:32 -0600

    added more decks

--- project1/js/app.js
+++ project1/js/app.js
@@ -1,35 +1,83 @@
 console.log('JS is loaded!');
 
-//Define A Card
-// $('card') = function(numerator, denominator, value, divider){
-// 	var numerator = [];
-// 	var denominator = [];
-// 	var divider = "/"
+//Dynamically Create A master deck and smaller decks for board
+// function buildDeck(){
+// 	var deckCount = 0;
+// 	var numerator = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+// 	var denominator = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 // 	var value = numerator/denominator;
-//}
+// 	var deck = [];
+// 	for (var n = 1; n <=numerator.length; n++){
+// 		for (var d = 1; d <=denominator.length; d++){
+// 			deck.push('text: "' + numerator[n] + '/' + denominator[d] + ', value: "' + numerator[n]/denominator[d])
+// 		}
+// 	} 
+// }
 
 //create Decks
 var decks = [
-[{text: "8/10", value: .8},{text: "3/4", value: .75}, {text: "1/5", value: .2}, 
-{text: "3/8", value: .375}, {text: "6/10", value: .6}, {text: "4/5", value: .8, ans:.8 }],
+[{text: "5/10", value: .5},{text: "3/4", value: .75}, {text: "1/5", value: .2}, 
+{text: "3/8", value: .375}, {text: "6/10", value: .6}, {text: "4/5", value: .8},
+{text: "3/7", value: .429 }, {text: "6/12", value: .5},{text: "4/8", value: .5},
+{text: "1/16", value: .063},{text: "3/15", value: .2}, {text: "4/5", value: .8}, 
+{text: "2/3", value: .667}, {text: "9/12", value: .75}, {text: "8/10", value: .8},
+{text: "1/2", value: .5}],
 
-[{text: "4/8", value: .5},{text: "5/1", value: 5}, {text: "100/200", value: .5}, 
-{text: "3/24", value: .125}, {text: "4/2", value: 2}, {text: "1/8", value: .125, ans:.125 }],
+[{text: "2/4", value: .5},{text: "3/1", value: 3}, {text: "10/20", value: .5},
+{text: "6/8", value: .75},{text: "6/2", value: 3}, {text: "1/2", value: .5},
+{text: "3/6", value: .5},{text: "1/9", value: .111}, {text: "11/12", value: .917},
+{text: "5/10", value: .5},{text: "9/12", value: .75}, {text: "6/7", value: .86}, 
+{text: "3/24", value: .125}, {text: "4/2", value: 2}, {text: "12/16", value: .75},
+{text: "3/4", value: .75},],
 
-[{text: "8/16", value: .5},{text: "19/20", value: .95}, {text: "12/16", value: .75}, 
-{text: "9/10", value: .9}, {text: "6/10", value: .6}, {text: "3/4", value: .75, ans:.75 }],
+[{text: "2/4", value: .5},{text: "1/2", value: .5}, {text: "3/5", value: .6},
+{text: "2/6", value: .333},{text: "3/9", value: .333}, {text: "6/9", value: .667},
+{text: "2/7", value: .286},{text: "3/4", value: .75}, {text: "5/6", value: .833},
+{text: "4/10", value: .4},{text: "7/8", value: .875}, {text: "5/15", value: .333}, 
+{text: "3/16", value: .188}, {text: "4/1", value: 4}, {text: "1/11", value: .091},
+{text: "1/3", value: .333},],
 
-[{text: "7/7", value: 1},{text: "7/21", value: .333}, {text: "7/28", value: .25}, 
-{text: "7/14", value: .5}, {text: "7/56", value: .125}, {text: "1/3", value: .333, ans:.333}],
+[{text: "1/8", value: .125},{text: "1/2", value: .5}, {text: "15/18", value: .833},
+{text: "2/3", value: .667},{text: "10/12", value: .833}, {text: "6/9", value: .667},
+{text: "6/10", value: .6},{text: "8/12", value: .667}, {text: "1/9", value: .111},
+{text: "6/2", value: 3},{text: "7/8", value: .875}, {text: "4/8", value: .5}, 
+{text: "25/30", value: .833}, {text: "4/1", value: 4}, {text: "1/11", value: .091},
+{text: "5/6", value: .833},],
 
-[{text: "3/5", value: .6},{text: "15/20", value: .80}, {text: "2/5", value: .4}, 
-{text: "3/8", value: .375}, {text: "4/4", value: 1}, {text: "8/20", value: .4, ans:.4 }],
+[{text: "5/10", value: .5},{text: "2/16", value: .125}, {text: "3/5", value: .6}, 
+{text: "4/8", value: .5}, {text: "6/10", value: .6}, {text: "4/5", value: .8},
+{text: "3/12", value: .25 }, {text: "8/12", value: .75},{text: "4/8", value: .5},
+{text: "1/16", value: .063},{text: "5/5", value: 1}, {text: "1/4", value: .25}, 
+{text: "2/3", value: .667}, {text: "9/12", value: .75}, {text: "5/20", value: .25},
+{text: "2/8", value: .25}],
 
-[{text: "3/20", value: .15},{text: "12/18", value: .667}, {text: "1/5", value: .2}, 
-{text: "3/9", value: .333}, {text: "4/10", value: .4}, {text: "6/9", value: .667, ans:.667 }],
+[{text: "4/20", value: .2},{text: "1/3", value: .333}, {text: "0/20", value: 0},
+{text: "5/8", value: .675},{text: "5/1", value: 5}, {text: "11/22", value: .917},
+{text: "3/5", value: .6},{text: "9/9", value: 1}, {text: "6/7", value: .86},
+{text: "3/10", value: .3},{text: "4/12", value: .333}, {text: "1/1", value: 1}, 
+{text: "6/24", value: .25}, {text: "1/2", value: .5}, {text: "6/20", value: .30},
+{text: "2/10", value: .20},],
 
-[{text: "7/20", value: .35},{text: "6/8", value: .75}, {text: "14/7", value: 2}, 
-{text: "6/16", value: .375}, {text: "3/5", value: .6}, {text: "6/3", value: 2, ans:2 }]
+[{text: "2/4", value: .5},{text: "1/2", value: .5}, {text: "3/5", value: .6},
+{text: "2/6", value: .333},{text: "3/9", value: .333}, {text: "6/9", value: .667},
+{text: "2/7", value: .286},{text: "3/4", value: .75}, {text: "5/6", value: .833},
+{text: "4/10", value: .4},{text: "7/8", value: .875}, {text: "5/15", value: .333}, 
+{text: "3/16", value: .188}, {text: "4/1", value: 4}, {text: "1/11", value: .091},
+{text: "1/8", value: .333},],
+
+[{text: "2/4", value: .5},{text: "1/2", value: .5}, {text: "3/5", value: .6},
+{text: "2/6", value: .333},{text: "3/9", value: .333}, {text: "6/9", value: .667},
+{text: "2/7", value: .286},{text: "3/4", value: .75}, {text: "5/6", value: .833},
+{text: "4/10", value: .4},{text: "7/8", value: .875}, {text: "5/15", value: .333}, 
+{text: "3/16", value: .188}, {text: "4/1", value: 4}, {text: "1/11", value: .091},
+{text: "1/9", value: .333},],
+
+[{text: "2/4", value: .5},{text: "1/2", value: .5}, {text: "3/5", value: .6},
+{text: "2/6", value: .333},{text: "3/9", value: .333}, {text: "6/9", value: .667},
+{text: "2/7", value: .286},{text: "3/4", value: .75}, {text: "5/6", value: .833},
+{text: "4/10", value: .4},{text: "7/8", value: .875}, {text: "5/15", value: .333}, 
+{text: "3/16", value: .188}, {text: "4/1", value: 4}, {text: "1/11", value: .091},
+{text: "1/10", value: .333},],
 
 ];
 
@@ -43,7 +91,17 @@ function newGame(){
 	$('#3').data('value',decks [deckNum][2].value).text(decks [deckNum][2].text); 
 	$('#4').data('value',decks [deckNum][3].value).text(decks [deckNum][3].text); 
 	$('#5').data('value',decks [deckNum][4].value).text(decks [deckNum][4].text); 
-	$('.playCard').data('value',decks [deckNum][5].value).text(decks [deckNum][5].text);
+	$('#6').data('value',decks [deckNum][5].value).text(decks [deckNum][5].text); 
+	$('#7').data('value',decks [deckNum][6].value).text(decks [deckNum][6].text);
+	$('#8').data('value',decks [deckNum][7].value).text(decks [deckNum][7].text); 
+	$('#9').data('value',decks [deckNum][8].value).text(decks [deckNum][8].text); 
+	$('#10').data('value',decks [deckNum][9].value).text(decks [deckNum][9].text);
+	$('#11').data('value',decks [deckNum][10].value).text(decks [deckNum][10].text); 
+	$('#12').data('value',decks [deckNum][11].value).text(decks [deckNum][11].text); 
+	$('#13').data('value',decks [deckNum][12].value).text(decks [deckNum][12].text); 
+	$('#14').data('value',decks [deckNum][13].value).text(decks [deckNum][13].text); 
+	$('#15').data('value',decks [deckNum][14].value).text(decks [deckNum][14].text);
+	$('.playCard').data('value',decks [deckNum][15].value).text(decks [deckNum][15].text);
 	deckNum++;
 }
 
@@ -57,7 +115,8 @@ function newGame(){
 // 	}
 // }
 
-// Initiate Game
+//Initiate Game
+//newGame();
 // $(document).ready() 
 // $('cards').hidden();
 // }
@@ -74,6 +133,10 @@ $('.score').text(score +=50);
 	$('.scoreValue').text(score);
 }
 
+function resetScore(){
+	$('.scoreValue').text(0000);
+}
+
 //PLAY THE GAME	- the clicking action the chooses fractions and changes board
 var score = 0;
 	$('.cards').click(function(e){
@@ -83,10 +146,11 @@ var score = 0;
 	if (clickValue === playValue){
 		$(this).effect('pulsate', {times:1}, 2000);
 		scoreIt();
+	}	
 //			alert('You Got It!')
-		newGame();				//called here gives game some continuous speed
-	}
-	else {
+	// newGame();				
+	else { 
+//		$('#buzz').play();
 		alert('try Again.');	
 	}
 });
@@ -100,16 +164,18 @@ countdown = function(){		//call when each deck is played
 			console.log(sec);
 			if (sec == 0){
 				$('.clockValue').text('TIMES UP!');
+				$('.next').hide();
 //				$('.clockValue').fadeOut('slow');
 				clearInterval(count);
 			}
-		}, 1000);			
+		}, 1000);	
+		resetScore();		
 }
-$('cards').hide();
 
 
 //CREATE BUTTONS - start, reset and change difficulty
-$('.start').click(function(e){			//starts clock and toggles buttons
+$('.start').click(function(e){			//starts clock and toggles buttons  
+	newGame();
 	countdown();
 	$('.start').hide();
 	$('.easy').hide();
@@ -123,18 +189,23 @@ $('.reset').click(function(e){			//refreshes page and restarts game
 })
 
 $('.easy').click(function(e){			//makes game easy
-	$('clockValue').text('25');
-	sec = 25;
+	$('clockValue').text('60');
+	sec = 60;
   
 	console.log('easy! timer set at: ' + sec + ' seconds');
 })
 
 $('.hard').click(function(e){			//makes game harder
-	sec = 15;
-	$('clockValue').text('15');
+	sec = 30;
+	$('clockValue').text('30');
 	console.log('hard! timer set at: ' + sec + ' seconds');
 
 })
+
+$('.next').click(function(e){
+//	shuffleDecks();
+	newGame();
+})	
 
 
 
