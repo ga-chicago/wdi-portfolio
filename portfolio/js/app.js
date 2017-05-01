ed6a03cb877819ac5396fad9b4338f539434347a
cSwormstedt <cswormstedt7@gmail.com>
2017-05-01 00:32:07 -0500

    styled

---
+++ portfolio/js/app.js
@@ -0,0 +1,13 @@
+// $('.downArrow').click(function() {
+//     var id = $(this).attr('id');
+//     $('html, body').animate({
+//         scrollTop: ($('#1' +).offset().top)
+//     }, 1000);
+// });
+
+// $('.downArrow2').click(function() {
+//     var id = $(this).attr('id');
+//     $('html, body').animate({
+//         scrollTop: ($('#2').offset().top)
+//     }, 1000);
+// });
